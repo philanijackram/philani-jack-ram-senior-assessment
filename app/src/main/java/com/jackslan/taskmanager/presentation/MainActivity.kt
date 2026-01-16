dc01b1d999e8e4f3562f64c003de58b470b4b067
Philani Jack r <philanijackr@gmail.com>
2026-01-16 20:24:41 +0200

    Initial Commit

---
+++ app/src/main/java/com/jackslan/taskmanager/presentation/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.jackslan.taskmanager.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.jackslan.taskmanager.presentation.features.home.HomeScreen
+import com.jackslan.taskmanager.presentation.theme.TaskManagerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TaskManagerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    HomeScreen(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/jackslan/taskmanager/presentation/features/home/HomeScreen.kt
@@ -0,0 +1,10 @@
+package com.jackslan.taskmanager.presentation.features.home
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HomeScreen(modifier: Modifier){
+
+}
+
